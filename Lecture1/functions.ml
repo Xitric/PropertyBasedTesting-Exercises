eb2e40b0e1a0224fb5df9e79a7a4c05ea5454d5f
Kasper Davidsen <kdavi16@student.sdu.dk>
2020-02-03 17:05:48 +0100

    Lecture 1 improved is_even

--- Lecture1/functions.ml
+++ Lecture1/functions.ml
@@ -4,11 +4,7 @@ print_endline "Cube:";;
 Printf.printf "%d\n" (cube 2);;
 Printf.printf "%d\n" (cube 3);;
 
-let is_even a =
-    if a mod 2 = 0 then
-        true
-    else
-        false;;
+let is_even a = a mod 2 = 0;;
 print_endline "\nEven:";;
 Printf.printf "%b\n" (is_even 3);;
 Printf.printf "%b\n" (is_even 4);;
