c3f5c8653ec7bf5bcfc844889fedeae720d99712
Kasper Davidsen <kdavi16@student.sdu.dk>
2020-02-10 14:34:25 +0100

    OCaml exercises for lecture 2

---
+++ Lecture2/patterns.ml
@@ -0,0 +1,8 @@
+(* Binding using pattern matching directly in the head *)
+let fst (x, _) = x
+
+(* Binding using pattern matching shorthand *)
+let snd = function (_, y) -> y
+
+(* Alternatively, we can write *)
+let first p = match p with (x, _) -> x
---
+++ Lecture2/recursion.ml
@@ -0,0 +1,13 @@
+
+(* A tail-recursive function using an accumulator *)
+(* Using optionals so that we do not need to pass the accumulator the first time we call the function *)
+let rec msb_acc ?(n=0) x = if x = 0 then
+        n
+    else
+        msb_acc ~n:(n + 1) (x lsr 1)
+
+(* A non-tail-recursive function that simply accumulates the value as the call stack unwraps *)
+let rec msb x = if x = 0 then
+        0
+    else
+        msb (x lsr 1) + 1
