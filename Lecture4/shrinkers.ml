79fc7ccbd3921d4e566c07b0e603a384ae0fc9de
Kasper Davidsen <kdavi16@student.sdu.dk>
2020-03-02 18:23:15 +0100

    Lecture 4 exercise 6

--- Lecture4/shrinkers.ml
+++ Lecture4/shrinkers.ml
@@ -26,3 +26,39 @@ let _ = QCheck_runner.run_tests [tb]
 (* Then it reduces the right value, which now bottoms out at 1 since (0,0) is not a counter example *)
 let pair_test = Test.make (pair small_nat small_nat) (fun (i,j) -> i+j = 0);;
 QCheck_runner.run_tests ~verbose:true [pair_test];;
+
+(* Exercise 6, a *)
+(* We iteratively try to cut the list in half, using the head and tail as smaller examples *)
+(* After this, we can just try to cut away the front element of the list *)
+(* Lastly, we can try to simplify the values inside the list *)
+
+(* Exercise 6, b *)
+let split_list l =
+    let length = List.length l in
+    let rec head l n =
+        if n = 0 then
+            []
+        else match l with
+            | [] -> []
+            | a::rest -> a::(head rest (n-1)) in
+    let rec tail l o n =
+        if n = 0 then
+            []
+        else match l with
+            | [] -> []
+            | a::rest -> if o > 0 then tail rest (o-1) n
+                        else a::(tail rest 0 (n-1)) in
+    let head_count = length lsr 1 in
+    (head l head_count, tail l head_count (length - head_count))
+
+let my_list_shrinker l = match l with
+    | [] -> Iter.empty (* No more elements *)
+    | a::[] -> Iter.return [] (* Only one element left *)
+    | _::rest -> let (head, tail) = split_list l in (* At least two elements left *)
+        Iter.of_list [head; tail; rest]
+
+let list_test_a = Test.make ~name:"List a" (set_shrink my_list_shrinker (list int)) (fun es -> List.rev es = es)
+let list_test_b = Test.make ~name:"List b" (set_shrink my_list_shrinker (list int)) (fun es -> List.length es < 5)
+let _ = QCheck_runner.run_tests [list_test_a; list_test_b]
+
+(* The shrinker seems to find minimal lists in terms of their length, but it does not yet minimize the elements of the lists *)
